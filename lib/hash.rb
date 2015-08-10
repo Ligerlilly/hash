caa1f090a63b56c239c39b11864f6679f2567b8e
Ryan & Jason Lane <student@epicodus.com>
2015-08-10 14:01:23 -0700

    add my_store, my_fetch, & my_has_key? methods

---
+++ lib/hash.rb
@@ -0,0 +1,23 @@
+
+
+class MyHash
+  define_method(:initialize) do
+    @key_array = []
+    @value_array = []
+  end
+
+  define_method(:my_store) do | key, value |
+    @key_array.push(key)
+    @value_array.push(value)
+    value
+  end
+
+  define_method(:my_fetch) do | key |
+    @value_array[@key_array.index(key)]
+  end
+
+  define_method(:my_has_key?) do | key |
+    @key_array.include?(key)
+  end
+
+end
---
+++ spec/hash_spec.rb
@@ -0,0 +1,26 @@
+require 'rspec'
+require './lib/hash'
+
+describe MyHash do
+  before do
+    @test_hash = MyHash.new
+  end
+  describe '#my_store' do
+    it 'takes two arguments ' do
+      expect(@test_hash.my_store('animal', 'dog')).to eq 'dog'
+    end
+  end
+  describe '#my_fetch' do
+    it 'takes two arguments ' do
+      @test_hash.my_store('animal', 'dog')
+      expect(@test_hash.my_fetch('animal')).to eq 'dog'
+    end
+  end
+
+  describe '#my_has_key?' do
+    it 'returns true is test_hash has key' do
+      @test_hash.my_store('animal', 'cat')
+      expect(@test_hash.my_has_key?('animal')).to eq true
+    end
+  end
+end
